9b14b2a12c3e3ff66272bd37c6d10d9828381d54
Leng <mghuy@unca.edu>
2017-05-03 23:58:42 -0400

    Updated server.js

--- core/serve.js
+++ core/serve.js
@@ -1,4 +1,9 @@
-/**
- * Created by TheEn on 3/28/2017.
- */
+var express = require('express');
+var bodyparser = require('body-parser');
+var app = express();
+
+app.use(express.static(__dirname));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.listen(3000);
 
